8e803d0b18fe682d6c3fda560d45d878733bf933
Benjamin Gutierrez <mrbengutierrez@gmail.com>
2025-06-18 00:36:26 -0400

    Update browser demo

--- docs/pkg/racket_rs_bg.wasm.d.ts
+++ docs/pkg/racket_rs_bg.wasm.d.ts
@@ -1,7 +1,9 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export const eval_racket: (a: number, b: number) => [number, number];
+export const __wbg_evalcontext_free: (a: number, b: number) => void;
+export const evalcontext_new: () => number;
+export const evalcontext_eval: (a: number, b: number, c: number) => [number, number];
 export const __wbindgen_export_0: WebAssembly.Table;
 export const __wbindgen_malloc: (a: number, b: number) => number;
 export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
