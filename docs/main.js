9f164c2f8652ca78adc8748eec80db1d0e5e1cd2
Benjamin Gutierrez <mrbengutierrez@gmail.com>
2025-06-18 00:00:47 -0400

    Update browser demo

--- docs/main.js
+++ docs/main.js
@@ -1,13 +1,20 @@
 import init, { eval_racket } from "./pkg/racket_rs.js";
 
+const evalBtn = document.querySelector("button");
+
+evalBtn.disabled = true;
+
 async function runWasm() {
     await init();
 
-    window.run = function () {
+    console.log("WASM loaded");
+    evalBtn.disabled = false;
+
+    evalBtn.addEventListener("click", () => {
         const input = document.getElementById("input").value;
         const output = eval_racket(input);
         document.getElementById("output").textContent = output;
-    };
+    });
 }
 
 runWasm();
