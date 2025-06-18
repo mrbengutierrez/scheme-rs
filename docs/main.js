c6dca03be5eaaec2e4f68ec3822905304b2564aa
Benjamin Gutierrez <mrbengutierrez@gmail.com>
2025-06-18 00:04:04 -0400

    Update browser demo

--- docs/main.js
+++ docs/main.js
@@ -1,16 +1,10 @@
 import init, { eval_racket } from "./pkg/racket_rs.js";
 
-const evalBtn = document.querySelector("button");
-
-evalBtn.disabled = true;
-
 async function runWasm() {
     await init();
+    console.log("✅ WASM initialized");
 
-    console.log("WASM loaded");
-    evalBtn.disabled = false;
-
-    evalBtn.addEventListener("click", () => {
+    document.getElementById("eval-button").addEventListener("click", () => {
         const input = document.getElementById("input").value;
         const output = eval_racket(input);
         document.getElementById("output").textContent = output;
